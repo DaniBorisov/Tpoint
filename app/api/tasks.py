865f73b5a46353cf892ae4cbe52e7b51786b5f9a
Daniel Borisov <dani.borisov@abv.bg>
2026-07-16 14:45:24 +0200

    added postgres and llm call

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -4,6 +4,9 @@ from fastapi import Depends
 from app.schemas.task import TaskCreate
 from app.services.task_service import TaskService
 
+from sqlalchemy.orm import Session
+from app.database.database import get_db
+
 def get_task_service():
     return TaskService()
 # service = TaskService()
@@ -18,6 +21,19 @@ def get_tasks(priority: str | None = None,
               service: TaskService = Depends(get_task_service)):
     return service.get_tasks(priority)
 
+@router.get("/sql")
+def get_tasks_sql(db: Session = Depends(get_db),
+                  service: TaskService = Depends(get_task_service)):
+    return service.get_tasks_sql(db)
+
+@router.post("/sql")
+def create_task_sql(
+        task: TaskCreate,
+        service: TaskService = Depends(get_task_service),
+        db: Session = Depends(get_db),
+        ):
+    return service.create_task_sql(task, db)
+
 @router.get("/{task_id}")
 def get_task(task_id: int,
              service: TaskService = Depends(get_task_service)):
@@ -27,3 +43,5 @@ def get_task(task_id: int,
 def create_task(task: TaskCreate,
                 service: TaskService = Depends(get_task_service)):
     return service.create_task(task)
+
+
