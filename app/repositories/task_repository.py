7e6cf9172cb7d7597890367e113c0d70da5993ce
Daniel Borisov <dani.borisov@abv.bg>
2026-07-16 19:08:32 +0200

    polishing up PostgreSQL usage and adding Repository

---
+++ app/repositories/task_repository.py
@@ -0,0 +1,40 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.models.task import Task
+
+class TaskRepository:
+
+    def get_all(
+            self,
+            db: Session,
+            priority: str | None = None,
+    ):
+        if priority is None:
+            return db.scalars(
+                select(Task)
+            ).all()
+        
+        return db.scalars(
+            select(Task).where(Task.priority == priority)
+        ).all()
+    
+    def get_task(
+            self,
+            db: Session,
+            task_id: int,
+             ):
+        return db.scalars(select(Task).where(Task.id == task_id)
+        ).all()
+    
+    def create_task(
+            self,
+            db: Session,
+            task: Task
+    ):
+        db.add(task)
+        db.commit()
+        db.refresh(task)
+
+        return task
+    
